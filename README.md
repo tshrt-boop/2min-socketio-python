6ed2ac2c5a20e36e5c77d57fc34cc8421d1fde81
tshrt <copyyyy@gmail.com>
2021-03-29 23:42:48 +0900

    サンプルうごいた

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # 2min-socketio-python
+
+## 環境構築
+
+```sh
+pipenv sync --dev
+pipenv shell
+```
+
+
+## Socket.IOでサーバーを立ててHoppscotchで接続
+
+```sh
+python server.py
+```
+
+Hoppscotchを起動、URLに`ws://localhost:8000`を入力して接続。
+
---
+++ server.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from sanic import Sanic
+from sanic.response import html
+
+import socketio
+
+sio = socketio.AsyncServer(async_mode='sanic', cors_allowed_origins=['https://hoppscotch.io'])
+app = Sanic(name='sio')
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
+sio.attach(app)
+
+
+@sio.event
+def connect(sid, environ, auth):
+    print('connect ', sid)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    app.run()
